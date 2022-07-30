497eadb01b3061c6d8af0febd2591f4c36639845
James Rhodes <vincerhodes@gmail.com>
2022-07-30 16:00:38 +0800

    Completed CRUD development

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -32,8 +32,9 @@ class StreamList extends Component {
         <div className='item' key={stream.id}>
           {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
+
           <div className='content'>
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
             <div className='description'>{stream.description}</div>
           </div>
         </div>
